8ff8b9477de29c16e5491f7e6c95c92cda86f92c
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2023-12-20 23:17:30 +0530

    Taking Basic Order Details

---
+++ AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  Cupcake Corner
+//
+//  Created by Sanjay Siddharth on 20/12/23.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}
---
+++ Cupcake Corner/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by Sanjay Siddharth on 20/12/23.
+//
+
+import Foundation
